827ebd2fad7e0073fb364acb96e1be2d155b44eb
Imron-DE <imronrosyadi97@gmail.com>
2025-04-22 19:36:49 +0700

    initial commit

---
+++ app/errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require("./custom-api-error");
+const BadRequest = require("./bad-request");
+const NotFound = require("./not-found");
+
+module.exports = { CustomAPIError, BadRequest, NotFound };

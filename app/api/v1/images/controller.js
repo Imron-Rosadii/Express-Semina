ca8375d23a436fa8262dd5a8bd5f1d15ed7b5c03
Imron-DE <imronrosyadi97@gmail.com>
2025-04-22 21:16:01 +0700

    update

---
+++ app/api/v1/images/controller.js
@@ -0,0 +1,14 @@
+const { StatusCodes } = require("http-status-codes");
+const { createImages } = require("../../../service/mongoose/images");
+
+const create = async (req, res, next) => {
+  try {
+    const result = await createImages(req);
+
+    res.status(StatusCodes.CREATED).json({ data: result });
+  } catch (error) {
+    next(err);
+  }
+};
+
+module.exports = { create };
